cdd20a9ac36d3920e7a847bfee59a217c11e98e1
18881408 <IVLobov@sberbank.ru>
2023-01-12 08:08:29 +0500

    Initial commit (testBot with mvn - v2)

---
+++ src/main/java/SpringBootTestBot/testBot/config/BotConfig.java
@@ -0,0 +1,19 @@
+package SpringBootTestBot.testBot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
+public class BotConfig {
+
+    @Value("${name}")
+    String botName;
+
+    @Value("${token}")
+    String token;
+
+}
---
+++ src/main/java/SpringBootTestBot/testBot/service/TelegramBot.java
@@ -0,0 +1,63 @@
+package SpringBootTestBot.testBot.service;
+
+import SpringBootTestBot.testBot.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config){
+        this.config = config;
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                default: sendMessage(chatId, "Саша самая красивая");
+                break;
+            }
+        }
+    }
+    public void startCommandReceived(long chatId, String name){
+        String answer = "test";
+        sendMessage(chatId, answer);
+    }
+
+    public void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException exception) {
+
+        }
+    }
+
+}
